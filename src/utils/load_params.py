b7a2be6825c78337f7a5a7a65173f1da19673392
Chris Thompson <chris.thompson2@insight.com>
2023-01-23 10:08:57 -0800

    add DVC pipeline

--- src/utils/load_params.py
+++ src/utils/load_params.py
@@ -4,5 +4,6 @@ from box import ConfigBox
 def load_params(params_path):
     with open(params_path, "r") as f:
         params = yaml.safe_load(f)
+        print(params)
         params = ConfigBox(params)
     return params
